77012ceb49d51bd5518caa668b6b5dfe8c5d7739
wanziee <ichwanalghifa@gmail.com>
2025-05-05 18:05:48 +0700

    Initial Commit

---
+++ CoinTrek/CoinTrekApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinTrekApp.swift
+//  CoinTrek
+//
+//  Created by mohammad ichwan al ghifari on 05/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CoinTrekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoinTrek/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoinTrek
+//
+//  Created by mohammad ichwan al ghifari on 05/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
